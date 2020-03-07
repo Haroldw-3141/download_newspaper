736e28678b44ebd5b2cb82d97ffe47ecf8f69652
Haroldw-3141 <61399337+Haroldw-3141@users.noreply.github.com>
2020-03-07 20:19:45 +0800

    Change relative directory syntax.

--- download_globaltimes.py
+++ download_globaltimes.py
@@ -8,7 +8,7 @@ import shutil
 import time
 
 host_link = "https://www.hqck.net"
-base_location = "~/Newspaper/"
+base_location = "./Newspaper/"
 my_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 Edg/80.0.361.50"
 }
